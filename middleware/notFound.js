c421995380292cac7cc7d41ba58653345bee35a4
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-13 01:51:59 +0300

    bug fix: adaug target pt linkul de home

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,5 +1,5 @@
 const notFound = (req, res) => {
-    res.send('Ups! Page doesn`t exist! <a>Back Home</a>');
+    res.send('Ups! Page doesn`t exist! <a href="/">Back Home</a>');
 }
 
 module.exports = notFound;
