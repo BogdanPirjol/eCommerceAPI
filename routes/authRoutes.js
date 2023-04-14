2b5b3cd8a0fecfdc912d6ebbed9f30025b1d7633
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 16:40:31 +0300

    rename authRouter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
-const authRouter = require('express').Router();
+const router = require('express').Router();
 const authControllers = require('../controllers/auhtController');
 
-authRouter.post('/login', authControllers.login);
-authRouter.post('/register', authControllers.register);
-authRouter.get('/logout', authControllers.logout);
+router.post('/login', authControllers.login);
+router.post('/register', authControllers.register);
+router.get('/logout', authControllers.logout);
 
-module.exports = authRouter;
+module.exports = router;
