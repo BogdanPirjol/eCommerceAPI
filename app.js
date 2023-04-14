5ceac2eb7c96281009524fdc12a9e8a58470021c
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 12:23:06 +0300

    adaug express.json middleware

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+//middleware
+app.use(express.json());
+
 //database
 const connectDB = require('./db/connectDB');
 
