2fe45fa92ecefa5ed4b91b607aa28a43e8f4593a
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 21:40:38 +0300

    add token sign to register

--- controllers/auhtController.js
+++ controllers/auhtController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const jsonwebtoken = require('jsonwebtoken');
 
 const login = (req, res) => {
     res.send('login fucntionality');
@@ -10,8 +11,10 @@ const logout = (req, res) => {
 }
 
 const register = async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json(user.dataValues);
+    const {name, email, password} = req.body;
+    const user = await User.create({name, email, password});
+    const token = jsonwebtoken.sign({user: user.name, email: user.email, userId: user.id}, process.env.JWT_SECRET, {expiresIn: '2h'});
+    res.status(StatusCodes.CREATED).json({"user": user.dataValues, token: token});
 }
 
 module.exports = {
