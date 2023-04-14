557472ab9a5d729defcb1494486306c2b6a5e59a
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 21:51:10 +0300

    refactor jwt logic

--- controllers/auhtController.js
+++ controllers/auhtController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const jsonwebtoken = require('jsonwebtoken');
+const { createToken } = require('../utils');
 
 const login = (req, res) => {
     res.send('login fucntionality');
@@ -11,10 +11,15 @@ const logout = (req, res) => {
 }
 
 const register = async (req, res) => {
-    const {name, email, password} = req.body;
-    const user = await User.create({name, email, password});
-    const token = jsonwebtoken.sign({user: user.name, email: user.email, userId: user.id}, process.env.JWT_SECRET, {expiresIn: '2h'});
-    res.status(StatusCodes.CREATED).json({"user": user.dataValues, token: token});
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    const payload = {
+        user: user.name,
+        userId: user.id,
+        role: user.role
+    };
+    const token = createToken({payload: payload, expiresIn: '2h'});
+    res.status(StatusCodes.CREATED).json({ "user": user.dataValues, token: token });
 }
 
 module.exports = {
