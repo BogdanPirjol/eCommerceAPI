3a8d0d8fa17c14d83c1c7b6872508c9a1d653d7b
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 17:03:33 +0300

    add bad request error

--- errors/index.js
+++ errors/index.js
@@ -1,9 +1,11 @@
 const CustomError = require('./customErrorAPI');
 const NotFoundError = require('./notFound');
 const UnauthenticatedError = require('./unauthenticated');
+const BadRequest = require('./BadRequest');
 
 module.exports = {
     CustomError,
     NotFoundError,
-    UnauthenticatedError
+    UnauthenticatedError, 
+    BadRequest
 }
