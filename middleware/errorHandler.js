bc9b53ca4794ddecc75dcc16b35dd69ae304b470
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-14 16:32:38 +0300

    add console.log(err)

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,7 @@ const { CustomError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
+    console.error(err.message, err.statusCode);
     if(err instanceof CustomError){
         res.status(err.statusCode).json(err.message);
     }
