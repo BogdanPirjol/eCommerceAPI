e5c8c5df1f944bd39224e5fd5472fe989f6506d2
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-18 19:55:48 +0300

    add attach cookie function

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const createToken = ({payload, expiresIn}) => {
-    console.log(payload);
+    //console.log(payload);
     return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: expiresIn});
 }
 
@@ -9,7 +9,20 @@ const isTokenValid = ({token}) => {
     return jwt.verify(token, process.env.JWT_SECRET);
 }
 
+const attachCookieToResponse = (res, user) => {
+    //console.log(tokenUser);
+    const oneDay = 1000 * 60 * 60 * 24;;
+    const cookieOptions = {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.ENVIROMENT === 'production',
+        signed: true
+    };
+    res.cookie('token', createToken({payload: user, expiresIn: oneDay}), cookieOptions);
+}
+
 module.exports = {
     createToken,
-    isTokenValid
+    isTokenValid,
+    attachCookieToResponse
 }
