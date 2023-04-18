4474db1da9d8390802d99d8d26b18c7ebaf7f2a1
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-18 21:50:30 +0300

    add user controller structure

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+
+
+const getAllUsers = async(req, res) => {
+
+}
+
+const getSingleUser = async(req, res) => {
+    
+}
+
+const showCurrentUser = async(req, res) => {
+    
+}
+
+const updateUser = async(req, res) => {
+    
+}
+
+const updateUsersPassword = async(req, res) => {
+    
+}
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    updateUsersPassword,
+    showCurrentUser
+}
