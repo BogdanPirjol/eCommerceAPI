ec87d4e89a68688e9229b5bae455d5aec47c1188
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-26 01:00:46 +0300

    add authentication and authorization to protected routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 const {
     createReview,
     getAllReviews,
@@ -6,7 +7,14 @@ const {
     updateReview,
     deleteReview } = require('../controllers/reviewController');
 
-router.route('/').get(getAllReviews).post(createReview);
-router.route('/:reviewId').get(getSingleReview).patch(updateReview).delete(deleteReview);
+
+//public routes
+router.route('/').get(getAllReviews);
+router.route('/:reviewId').get(getSingleReview);
+
+//protected routes
+router.use(authenticateUser, authorizePermissions('user'));
+router.route('/').post(createReview);
+router.route('/:reviewId').patch(updateReview).delete(deleteReview);
 
 module.exports = router;
