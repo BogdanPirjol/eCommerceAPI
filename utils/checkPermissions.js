e609586f50d70193e2dacf10f337569c6c5e273b
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-26 17:22:02 +0300

    add function for verifying permision for submiting user

---
+++ utils/checkPermissions.js
@@ -0,0 +1,9 @@
+const { UnauthorizedError } = require("../errors");
+
+const checkPermissions = (requestUser, resourceUserId) => {
+    if(requestUser.role === 'admin' || resourceUserId === requestUser.id)
+        return
+    throw new UnauthorizedError('Not authorized to perform this action');
+}
+
+module.exports = checkPermissions;
