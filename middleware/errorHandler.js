15b5e5ca2e17829e54ce629f268dee48d768a332
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-26 15:36:10 +0300

    modify error response to send error details for postgress constraint error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,12 +2,21 @@ const { CustomError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.error(err.message, err.statusCode);
-    if(err instanceof CustomError){
+    //console.error(err.message, err.original.message);
+    if (err instanceof CustomError) {
         res.status(err.statusCode).json(err.message);
     }
-    else{
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err.message);
+    else {
+        let errorDetails = {};
+        if (err.name.startsWith('Sequelize'))
+            errorDetails = {
+                message: err.message + ': ' + err.original.message,
+                details: err.original.detail,
+                parameters: err.original.parameters
+            }
+        else
+            errorDetails = err;
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(errorDetails);
     }
 }
 
