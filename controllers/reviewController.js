d3a5df2e19808abd9596dd7c05d53909190163f1
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-26 15:38:15 +0300

    add create review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,14 +1,32 @@
 const Review = require('../models/Review');
 const User = require('../models/User');
 const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequest } = require('../errors');
 
 
 const createReview = async (req, res) => {
-    res.send('createReview');
+    const { id: userId } = req.user;
+    if(!req.body.productId)
+        throw new BadRequest('Please provide productId!');
+    req.body.userId = userId;
+    const review = await Review.create(req.body);
+    res.status(StatusCodes.CREATED).json(review);
 }
 
 const getAllReviews = async (req, res) => {
-    res.send('getAllReviews');
+    const reviews = await Review.findAll({
+        include: [ Product, {
+            model: User,
+            attributes: {
+                exclude: ['password']
+            }
+        }]
+    });
+    res.status(StatusCodes.OK).json({
+        entries: reviews.length,
+        reviews
+    });
 }
 
 const getSingleReview = async (req, res) => {
