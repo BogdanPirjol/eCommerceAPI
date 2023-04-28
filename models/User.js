80122772d946db1610439512323b2535c36a5e60
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-28 18:45:14 +0300

    modify user model by extinding model class

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
-const sequelize = require('../db/connectDB');
-const { DataTypes } = require('sequelize');
+const sequelize = require('../db/connectDB')();
+const { DataTypes, Model } = require('sequelize');
 const bcrypt = require('bcrypt');
 
-const User = sequelize().define('User',
+/* const User = sequelize().define('User',
     {
         id: {
             type: DataTypes.UUID,
@@ -34,6 +34,10 @@ const User = sequelize().define('User',
     }, {
     hooks: {
         beforeCreate: async (user) => {
+            console.log({
+                this: this,
+                user: user
+            });
             const salt = await bcrypt.genSalt();
             const hashedPassword = await bcrypt.hash(user.password, salt);
             user.password = hashedPassword;
@@ -51,6 +55,51 @@ const User = sequelize().define('User',
 User.prototype.checkPassword = async function (passowrd) {
     const isMatch = await bcrypt.compare(passowrd, this.dataValues.password);
     return isMatch;
+} */
+
+class User extends Model {
+    static isFirstUser = true;
+    static checkFirstUser() {
+        if (this.isFirstUser) {
+            this.isFirstUser = false;
+            return true
+        }
+        return false;
+    }
 }
+
+User.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [2, 50]
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        },
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [6, 100]
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'user'
+    }
+},
+    {
+        sequelize,
+        modelName: 'User'
+    })
 
 module.exports = User;
