3d14328fa0c0eff570e3b8f188a7967f292274ac
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-28 18:52:47 +0300

    add hooks to user model

--- models/User.js
+++ models/User.js
@@ -2,61 +2,6 @@ const sequelize = require('../db/connectDB')();
 const { DataTypes, Model } = require('sequelize');
 const bcrypt = require('bcrypt');
 
-/* const User = sequelize().define('User',
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            len: [2, 50]
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isEmail: true
-            },
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            len: [6, 100]
-        },
-        role: {
-            type: DataTypes.STRING,
-            defaultValue: 'user'
-        }
-    }, {
-    hooks: {
-        beforeCreate: async (user) => {
-            console.log({
-                this: this,
-                user: user
-            });
-            const salt = await bcrypt.genSalt();
-            const hashedPassword = await bcrypt.hash(user.password, salt);
-            user.password = hashedPassword;
-        },
-        beforeUpdate: async (user, options) => {
-            if (options.fields.includes('password')) {
-                const salt = await bcrypt.genSalt();
-                const hashedPassword = await bcrypt.hash(user.password, salt);
-                user.password = hashedPassword;
-            }
-        }
-    }
-});
-
-User.prototype.checkPassword = async function (passowrd) {
-    const isMatch = await bcrypt.compare(passowrd, this.dataValues.password);
-    return isMatch;
-} */
-
 class User extends Model {
     static isFirstUser = true;
     static checkFirstUser() {
@@ -99,7 +44,21 @@ User.init({
 },
     {
         sequelize,
-        modelName: 'User'
-    })
+        modelName: 'User',
+        hooks: {
+            beforeCreate: async (user) => {
+                const salt = await bcrypt.genSalt();
+                const hashedPassword = await bcrypt.hash(user.password, salt);
+                user.password = hashedPassword;
+            },
+            beforeUpdate: async (user, options) => {
+                if (options.fields.includes('password')) {
+                    const salt = await bcrypt.genSalt();
+                    const hashedPassword = await bcrypt.hash(user.password, salt);
+                    user.password = hashedPassword;
+                }
+            }
+        }
+    });
 
 module.exports = User;
