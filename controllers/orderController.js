79c2a80ccec51db25f6c2d6e146a9e4c850b7c06
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-04-30 21:21:46 +0300

    add orders controller file

---
+++ controllers/orderController.js
@@ -0,0 +1,24 @@
+
+const getAllOrders = async (req, res) => {
+
+}
+const getSingeOrder = async (req, res) => {
+    
+}
+const getCurrentUserOrders = async (req, res) => {
+    
+}
+const createOrder = async (req, res) => {
+    
+}
+const updateOrder = async (req, res) => {
+    
+}
+
+module.exports = {
+    getAllOrders,
+    getSingeOrder,
+    getCurrentUserOrders,
+    createOrder,
+    updateOrder
+}
